fc5cc95776a9b206640a9de8e80ccc33cac78555
lizhuo-1994 <guitar_318576604@yahoo.co.jp>
2022-11-29 16:52:35 +0900

    update

--- train_drqv2.sh
+++ train_drqv2.sh
@@ -1,24 +1,33 @@
-
-
-python train.py task=humanoid_walk
-python necsa_train.py task=humanoid_walk
-
-python train.py task=humanoid_walk
-python necsa_train.py task=humanoid_walk
-
-python train.py task=humanoid_walk
-python necsa_train.py task=humanoid_walk
-
 # python train.py task=walker_walk
+# python train.py task=walker_walk
+# python train.py task=walker_walk
+# python train.py task=walker_walk
+# python train.py task=walker_walk
+# python necsa_train.py task=walker_walk
+# python necsa_train.py task=walker_walk
+# python necsa_train.py task=walker_walk
+# python necsa_train.py task=walker_walk
 # python necsa_train.py task=walker_walk
 
 # python train.py task=hopper_stand
 # python train.py task=hopper_stand
+# python train.py task=hopper_stand
+# python train.py task=hopper_stand
+# python train.py task=hopper_stand
 # python necsa_train.py task=hopper_stand
 # python necsa_train.py task=hopper_stand
-# python train.py task=walker_walk
-# python necsa_train.py task=walker_walk
-# python train.py task=walker_walk
-# python necsa_train.py task=walker_walk
-# python train.py task=walker_walk
-# python necsa_train.py task=walker_walk
+# python necsa_train.py task=hopper_stand
+# python necsa_train.py task=hopper_stand
+# python necsa_train.py task=hopper_stand
+
+
+python train.py task=humanoid_walk
+python train.py task=humanoid_walk
+python train.py task=humanoid_walk
+python train.py task=humanoid_walk
+python train.py task=humanoid_walk
+python necsa_train.py task=humanoid_walk
+python necsa_train.py task=humanoid_walk
+python necsa_train.py task=humanoid_walk
+python necsa_train.py task=humanoid_walk
+python necsa_train.py task=humanoid_walk
--- train_necsa_drqv2.sh
+++
@@ -1,8 +0,0 @@
-# python necsa_train.py task=walker_walk
-# python necsa_train.py task=walker_walk
-
-# python necsa_train.py task=hopper_stand
-# python necsa_train.py task=hopper_stand
-
-python necsa_train.py task=humanoid_walk
-python necsa_train.py task=humanoid_walk
